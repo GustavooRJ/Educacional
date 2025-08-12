c9c576e84cf58e0c6aef64836ca51e7d0afae8c6
202310541 <202310541@TPCP04LAB1106>
2025-08-11 21:01:41 -0300

    Adicionar arquivos de projeto.

---
+++ Academico/Models/Aluno.cs
@@ -0,0 +1,13 @@
+﻿namespace Academico.Models
+{
+    public class Aluno
+    {
+        public int Nome { get; set; }
+
+        public int Email { get; set; }
+
+        public int Cep { get; set; }
+
+
+    }
+}
