80b1d3d4a86c88e4dac73a04eefb6abc7cde5f8e
GustavooRJ <gugu.oliveira2012.go@gmail.com>
2025-09-01 09:17:29 -0300

    Adicionado CRUD departamento

---
+++ Academico/Controllers/DepartamentoController.cs
@@ -0,0 +1,86 @@
+﻿using Academico.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Academico.Controllers
+{
+    public class DepartamentoController: Controller
+    {
+        public static List<Departamento> _departamento = new List<Departamento>();
+        public IActionResult Index()
+        {
+            return View(_departamento);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Departamento departamento)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    departamento.DepartamentoID = _departamento.Select(d => d.DepartamentoID).DefaultIfEmpty(0).Max() + 1;
+                    _departamento.Add(departamento);
+                    return RedirectToAction("Index");
+                }
+                return View(departamento);
+            }
+            catch
+            {
+                return View(departamento);
+            }
+        }
+        public IActionResult Edit(int id)
+        {
+            var departamento = _departamento.FirstOrDefault(d => d.DepartamentoID == id);
+            if (departamento == null)
+            {
+                return NotFound();
+            }
+            return View(departamento);
+        }
+        [HttpPost]
+        public IActionResult Edit(Departamento departamento)
+        {
+            if (ModelState.IsValid)
+            {
+                _departamento.Remove(_departamento.Where(d => d.DepartamentoID == departamento.DepartamentoID).First());
+                _departamento.Add(departamento);
+                return RedirectToAction("Index");
+            }
+            return View(departamento);
+        }
+        public IActionResult Details(int id)
+        {
+            var departamento = _departamento.FirstOrDefault(d => d.DepartamentoID == id);
+            if (departamento == null)
+            {
+                return NotFound();
+            }
+            return View(departamento);
+        }
+        public IActionResult Delete(int id)
+        {
+            var departamento = _departamento.FirstOrDefault(d => d.DepartamentoID == id);
+            if (departamento == null)
+            {
+                return NotFound();
+            }
+            return View(departamento);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var departamento = _departamento.FirstOrDefault(d => d.DepartamentoID == id);
+            if (departamento != null)
+            {
+                _departamento.Remove(departamento);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
+
---
+++ Academico/Models/Departamento.cs
@@ -0,0 +1,10 @@
+﻿namespace Academico.Models
+{
+    public class Departamento
+    {
+        public int DepartamentoID { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public int Telefone { get; set; }
+        public List<Aluno> Alunos { get; set; } = new List<Aluno>();
+    }
+}
